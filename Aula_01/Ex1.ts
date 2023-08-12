635be9134bae59b22202d913bd69a99ad9feb710
vbaldochi <vbaldochi@hotmail.com>
2023-08-11 21:04:15 -0300

    Aula 2

---
+++ Aula_01/Ex1.ts
@@ -0,0 +1,8 @@
+namespace ex1{
+    let n1: number, n2: number, n3: number, n4: number;
+
+    let resultado : number;
+
+    resultado = n1 + n2 + n3 + n4;
+
+}
---
+++ Aula_01/exemplo2.ts
@@ -0,0 +1,19 @@
+namespace exemplo2{
+     // VAR / LET / CONST
+     let numero: number;
+     numero = 5;
+
+     let resultado: number;
+     resultado = numero + numero;
+     console.log("A soma entre ", + numero + " com " + numero + " é igual a " + resultado);
+
+     resultado = numero - 4;
+     console.log(`A subtração entre ${numero} com 4 é igual a: ${resultado}`);
+
+     resultado = numero * 2;
+     console.log(`A multiplicação entre ${numero} vezes 2 é igual a: ${resultado}`);
+
+     resultado = numero / 2;
+     console.log(`A divisão entre ${numero} divido por 2 é igual a: ${resultado}`);
+    
+}
